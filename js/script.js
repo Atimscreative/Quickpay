39aa7000503f2c1c3092b53cb4e8ffd4435589bb
Abegunde Timilehin <timilehinsunday144@gmail.com>
2023-05-22 13:52:25 +0100

    First files upload

---
+++ js/script.js
@@ -0,0 +1,126 @@
+"use strict";
+
+// PRELOADER
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loader");
+  // Remove message after 5 seconds
+  setTimeout(() => {
+    loader.classList.add("hidden");
+  }, 2000);
+});
+
+// Navigation
+const openMenu = document.querySelector(".open-menu"),
+  closeMenu = document.querySelector(".close-menu"),
+  navMenu = document.querySelector(".nav-menu"),
+  logo = document.querySelector(".logo");
+
+// Function that closes the nav menu
+function closeNavMenu() {
+  navMenu.classList.remove("display-mobile-nav");
+}
+
+// Navmenu Opens
+openMenu.addEventListener("click", function () {
+  navMenu.classList.add("display-mobile-nav");
+  logo.style.zIndex = "2";
+  logo.style.animation = "fade-in 300ms 1 ease-in-out";
+});
+
+// Navmenu Closes
+closeMenu.addEventListener("click", () => {
+  closeNavMenu();
+  logo.style.animation = "none";
+});
+
+// Remove Nav-Menu when link are clicked
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    closeNavMenu();
+  });
+});
+
+// LOGIN AND DOWNLOAD BTN
+const loginBtn = document.querySelector(".login-btn");
+
+// CHANGE HEADER BG AND ADD SHADOW ON SCROLL
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  const currentPosition = window.pageYOffset;
+
+  if (currentPosition >= 100) {
+    header.classList.add("header");
+  } else {
+    header.classList.remove("header");
+  }
+});
+
+// SCROLL BACK TO TOP
+const toTop = document.getElementById("back-to-top");
+window.addEventListener("scroll", function () {
+  if (this.scrollY >= 200) {
+    toTop.style.bottom = "10px";
+  } else {
+    toTop.style.bottom = "-50px";
+  }
+});
+
+/* ========= ACTIVE MENU SCROLLPSY ========= */
+// Attach a scroll event listener to the window object
+window.addEventListener("scroll", function () {
+  // Get the current scroll position of the window
+  var currentScrollPos = window.pageYOffset;
+
+  // Define the IDs of the selected sections
+  var sectionIds = ["hero", "whyus", "about", "contact"];
+
+  // Loop through the selected section IDs
+  for (var i = 0; i < sectionIds.length; i++) {
+    var sectionId = sectionIds[i];
+
+    // Get the corresponding section element by its ID
+    var section = document.getElementById(sectionId);
+
+    // Get the position and height of the section
+    var sectionPos = section.offsetTop - 50;
+    var sectionHeight = section.offsetHeight;
+
+    // Check if the current scroll position is within the range of the section
+    if (
+      currentScrollPos >= sectionPos &&
+      currentScrollPos < sectionPos + sectionHeight
+    ) {
+      // Add an "active" class to the corresponding link in the navigation menu
+      var navLink = document.querySelector("nav a[href='#" + sectionId + "']");
+      navLink.classList.add("active");
+    } else {
+      // Remove the "active" class from the corresponding link in the navigation menu
+      var navLink = document.querySelector("nav a[href='#" + sectionId + "']");
+      navLink.classList.remove("active");
+    }
+  }
+});
+// const sections = document.querySelectorAll("section[id]");
+
+// function scrollActive() {
+//   const scrollY = window.pageYOffset;
+
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight;
+//     const sectionTop = current.offsetTop - 50;
+//     const sectionId = current.getAttribute("id");
+
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document
+//         .querySelector("nav a[href='#" + sectionId + "']")
+//         .classList.add("active");
+//     } else {
+//       document
+//         .querySelector("nav a[href='#" + sectionId + "']")
+//         .classList.remove("active");
+//     }
+//   });
+// }
+// window.addEventListener("scroll", scrollActive);
---
+++ sw.js
@@ -0,0 +1,57 @@
+const staticCacheName = "site-static-v1.0";
+// const dynamicCacheName = "site-dynamic-v1";
+const assests = [
+  "/",
+  "/index.html",
+  "/js/script.js",
+  "/js/app.js",
+  "/css/style.css",
+  "/images/cbe018152347725.63362f541803f.png",
+  "/images/Logo.png",
+  "images/loader-img.png",
+  "images/phone.webp",
+  "images/left.png",
+  "images/right.png",
+  "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css",
+  "https://fonts.cdnfonts.com/css/cabinet-grotesk",
+  "https://fonts.cdnfonts.com/s/85514/CabinetGrotesk-Regular.woff",
+  " https://fonts.cdnfonts.com/s/85514/CabinetGrotesk-Bold.woff ",
+  "https://unpkg.com/boxicons@2.1.4/fonts/boxicons.woff2 ",
+  " https://fonts.cdnfonts.com/s/85514/CabinetGrotesk-Medium.woff",
+  "https://fonts.cdnfonts.com/s/85514/CabinetGrotesk-Extrabold.woff",
+];
+
+// Install Service Worker
+self.addEventListener("install", (evt) => {
+  // console.log("service worker has been installed");
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assests);
+    })
+  );
+});
+
+// Activate Service Worker Event
+self.addEventListener("activate", (evt) => {
+  // console.log("service worker has been activated");
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener("fetch", (evt) => {
+  // console.log("fetch event", evt);
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
